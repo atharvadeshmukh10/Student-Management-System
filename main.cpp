220d213250e2a1a40d020e38a437ced4bc6dceb4
atharvadeshmukh10 <atharvadeshmukh2547@gmail.com>
2026-03-14 20:17:01 +0530

    Made changes in main.cpp
    Removed the input method from the class.

--- main.cpp
+++ main.cpp
@@ -7,17 +7,6 @@ public:
     string name;
     int age;
 
-    void input() {
-        cout << "Enter ID: ";
-        cin >> id;
-
-        cout << "Enter Name: ";
-        cin >> name;
-
-        cout << "Enter Age: ";
-        cin >> age;
-    }
-
     void display() {
         cout << "ID: " << id << endl;
         cout << "Name: " << name << endl;
